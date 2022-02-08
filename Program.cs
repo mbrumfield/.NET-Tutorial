2958b2bb5f5d63460da89b541b79be4df49613a1
Marcus Brumfield <brumfieldmarcus@gmail.com>
2022-02-08 16:43:49 -0600

    Adding comments to Hello World file.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
+// Program to display Hello World!
+
 using System; 
 
+// Namespace declaration
 namespace MyNewApp
-{
+{   
+    // Class declaration 
     class Program
     {
+        // Main method 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
+            // Prevent the screen from running and closing quickly
             Console.ReadKey();
         }
     }
